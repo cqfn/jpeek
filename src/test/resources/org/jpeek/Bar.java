b0ae45145bf3b1cb70e1adacf9924d2423c3d1ae
Yildirim <MYildirim@agtinternational.com>
2017-10-29 20:43:25 +0100

    Change method

--- src/test/resources/org/jpeek/Bar.java
+++ src/test/resources/org/jpeek/Bar.java
@@ -1,22 +1,12 @@
 public final class Bar {
-
-    private final Object key;
-
-    private final Object value;
-
-    public Bar(final Object keyy, final Object val) {
-        this.key = keyy;
-        this.value = val;
-    }
-
+    private Object key;
+    private Object value;
     public Object getKey() {
         return this.key;
     }
-
     public Object getValue() {
         return this.value;
     }
-
     public Object setValue(final Object val) {
         throw new UnsupportedOperationException("This object is immutable.");
     }
