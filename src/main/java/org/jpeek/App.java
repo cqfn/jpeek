5caf2a71ad675c09c02e94ef27a9d67bd4745475
Kapralov Sergey <skapralov@mail.ru>
2018-01-24 22:10:18 +0300

    #9 TCC and LCC were introduced

--- src/main/java/org/jpeek/App.java
+++ src/main/java/org/jpeek/App.java
@@ -115,7 +115,9 @@ public final class App {
         this.save(skeleton.toString(), "skeleton.xml");
         final Iterable<Report> reports = new ListOf<>(
             new Report(skeleton, "LCOM", this.params, 10.0d, -5.0d),
-            new Report(skeleton, "MMAC", this.params, 0.5d, 0.25d)
+            new Report(skeleton, "MMAC", this.params, 0.5d, 0.25d),
+            new Report(skeleton, "TCC",  this.params, 0.5d, 0.25d),
+            new Report(skeleton, "LCC",  this.params, 0.5d, 0.25d)
         );
         new IoCheckedScalar<>(
             new AndInThreads(
