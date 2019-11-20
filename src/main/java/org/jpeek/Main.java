22023ab28df2865ffd2bbd05ccaf254194214a95
Gnusin Pavel <pavel.sin.gnusin@gmail.com>
2019-11-20 08:24:08 +0300

    yegor256#308 leading spaces

--- src/main/java/org/jpeek/Main.java
+++ src/main/java/org/jpeek/Main.java
@@ -43,9 +43,9 @@ import org.apache.log4j.PatternLayout;
  * @version $Id$
  * @since 0.1
  * @todo #308:30min Due to introduce new version of qulice 0.18.17 there were
- * many 'JavadocTagsCheck' suppression, so we need to fix that by remove
- * author, version and since tags in all javadoc. After that remove
- * 'JavadocTagsCheck' suppression.
+ *  many 'JavadocTagsCheck' suppression, so we need to fix that by remove
+ *  author, version and since tags in all javadoc. After that remove
+ *  'JavadocTagsCheck' suppression.
  * @checkstyle JavadocVariableCheck (500 lines)
  * @checkstyle JavadocTagsCheck (500 lines)
  */
