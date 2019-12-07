8630cbfa40a0ebfbcd17a04e05d826c95bfdf0d7
Yegor Bugayenko <yegor256@gmail.com>
2019-12-07 21:17:31 +0300

    #362 asm 7.2

--- src/main/java/org/jpeek/skeleton/XmlClass.java
+++ src/main/java/org/jpeek/skeleton/XmlClass.java
@@ -76,7 +76,7 @@ final class XmlClass extends ClassVisitor implements Iterable<Directive> {
      * @param src The source
      */
     XmlClass(final CtClass src) {
-        super(Opcodes.ASM6);
+        super(Opcodes.ASM7);
         this.source = src;
         this.attrs = new Directives();
         this.methods = new LinkedList<>();
