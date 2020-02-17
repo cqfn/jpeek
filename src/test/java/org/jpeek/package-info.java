f36123d91a53ba8adc6e4ca921bfd65c2a4035e4
Hamdi Douss <douss.hamdi@gmail.com>
2020-02-17 22:19:13 +0100

    #323 remove version id

--- src/test/java/org/jpeek/package-info.java
+++ src/test/java/org/jpeek/package-info.java
@@ -26,7 +26,7 @@
  * Tests.
  *
  * @author Yegor Bugayenko (yegor256@gmail.com)
- * @version $Id: 5f32b906da657837e39669980f837afea3dc7a9e $
+ * @version $Id$
  * @since 0.1
  * @todo #323:30min Migrate the tests in the sub-packages: org.jpeek.metrics,
  * org.jpeek.skeleton and org.jpeek.web to junit 5, so they won't
