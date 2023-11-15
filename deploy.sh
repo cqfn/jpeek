644b1542a586f15bb5593405c6e48618d20b6376
Yegor Bugayenko <yegor256@gmail.com>
2023-11-15 09:27:51 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd $(dirname $0)
-cp /code/home/assets/jpeek/settings.xml .
+cp /code/cqfn-secrets/jpeek-settings-heroku.xml settings.xml
 git add settings.xml
 git commit -m 'settings.xml for dokku'
 trap 'git reset HEAD~1 && rm settings.xml' EXIT
