33ac1fa3f87a5576851275d0878f71b110d1d64d
metafates <vvz.dev@gmail.com>
2024-02-11 18:09:10 +0300

    chore: apply fixes from shellcheck

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cd $(dirname $0)
+cd "$(dirname "$0")"
 cp /code/cqfn-secrets/jpeek-settings-heroku.xml settings.xml
 git add settings.xml
 git commit -m 'settings.xml for dokku'
--- research/2018-April/04-collect-metrics/process-artifact.sh
+++ research/2018-April/04-collect-metrics/process-artifact.sh
@@ -32,12 +32,12 @@ jar=$1
 output=$3
 path=${2//.//}
 meta=$(curl --fail --silent "https://repo1.maven.org/maven2/${path}/maven-metadata.xml")
-version=$(echo ${meta} | xmllint --xpath '/metadata/versioning/latest/text()' -)
-group=$(echo ${meta} | xmllint --xpath '/metadata/groupId/text()' -)
-artifact=$(echo ${meta} | xmllint --xpath '/metadata/artifactId/text()' -)
+version=$(echo "${meta}" | xmllint --xpath '/metadata/versioning/latest/text()' -)
+artifact=$(echo "${meta}" | xmllint --xpath '/metadata/artifactId/text()' -)
 
 home=$(pwd)
 dir=$(mktemp -d /tmp/jpeek-XXXX)
+# shellcheck disable=SC2064
 trap "rm -rf ${dir}" EXIT
 curl --fail --silent "https://repo1.maven.org/maven2/${path}/${version}/${artifact}-${version}.jar" > "${dir}/${artifact}.jar"
 cd "${dir}"
@@ -46,4 +46,4 @@ unzip -o -q -d "${artifact}" "${artifact}.jar"
 java -jar "${jar}" --sources "${artifact}" --target ./target --quiet
 ruby "${home}/collect.rb" target >> "${output}"
 cd
-rm -rf ${dir}
+rm -rf "${dir}"
